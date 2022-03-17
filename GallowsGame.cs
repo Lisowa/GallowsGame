2e1ad8edfa842636a44e8969d731eb3fdbea6bfb
Denisova Elizaveta <denisowaliza9@gmail.com>
2022-03-17 13:26:08 +0300

    Add project files.

---
+++ GallowsGame.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Gallows
+{
+    class GallowsGame
+    {
+        public int NumberOfAtempts { get; }
+        public int NumberOfErrors { get; private set; }
+        public string WordForGame { get; private set; }
+
+        private readonly char[] wordForGame;
+        private readonly char[] currentString;
+        private readonly string fileName;
+
+        public GallowsGame(int numberOfAtempts = 6, string fileName = "WordsStockRus.txt")
+        {
+            NumberOfAtempts = numberOfAtempts;
+            NumberOfErrors = 0;
+            this.fileName = fileName;
+            GenerateWordForGame();
+            wordForGame = WordForGame.ToCharArray();
+            currentString = GetStartString();            
+        }
+
+        public string MakeMove(char letter)
+        {
+            if (wordForGame.Contains(letter))
+            {
+                for (int i = 0; i < currentString.Length; i++)
+                    if (wordForGame[i] == letter)
+                        currentString[i] = letter;
+            }
+            else
+                NumberOfErrors++;
+
+            return new string(currentString);
+        }
+
+        public bool IsAttemptsEnded()
+        {
+            return (NumberOfErrors >= NumberOfAtempts);
+        }
+
+        public bool IsWordGuessed()
+        {
+            string currentWord = new string(currentString);
+            return currentWord.Equals(WordForGame);
+        }
+
+        public char[] GetStartString()
+        {
+            int lenght = wordForGame.Length;
+            char[] startString = new char[lenght];
+            for (int i = 0; i < lenght; i++)
+                startString[i] = '-';
+
+            return startString;
+        }
+
+        private void GenerateWordForGame()
+        {
+            string[] words = File.ReadAllLines(fileName);
+            int indexOfWord = new Random().Next(words.Length - 1);
+            WordForGame = words[indexOfWord];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Gallows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char enteredLetter;
+            bool isCorrectInput;
+            GallowsGame game = new GallowsGame();
+            Console.WriteLine(game.GetStartString());
+            while (!game.IsAttemptsEnded() && !game.IsWordGuessed())
+            {
+                Console.WriteLine($"You have {game.NumberOfAtempts - game.NumberOfErrors} attempts.");
+                do
+                {
+                    Console.Write("Enter letter: ");
+                    string enteredString = Console.ReadLine();
+                    enteredLetter = enteredString.ToLower().ToCharArray()[0];
+                    isCorrectInput = enteredString.Length == 1 && (enteredLetter >= 'а') && (enteredLetter <= 'я');
+                    if (!isCorrectInput)
+                        Console.WriteLine("Use cyrillic letter!");
+                }
+                while (!isCorrectInput);
+                Console.WriteLine(game.MakeMove(enteredLetter));
+            }
+            Console.WriteLine(game.IsWordGuessed() ? "\nYou won!" : $"\nYou lose! Word was {game.WordForGame}. Number of errors = {game.NumberOfErrors}");
+        }
+    }
+}
